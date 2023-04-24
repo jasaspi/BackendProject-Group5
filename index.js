4e80aa2768e23f8a282c279b4f2134e36f009ae1
lehtite <tero.lehtinen@logisnext.eu>
2023-04-24 17:56:48 +0300

    Added electric prices and sorted those

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+// Nordic electric prices
+const apiUrl = 'https://api.porssisahko.net/v1/latest-prices.json';
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Sort the data by price in ascending order
+    data.prices.sort((a, b) => a.price - b.price);
+
+    console.log(data);
+  });
