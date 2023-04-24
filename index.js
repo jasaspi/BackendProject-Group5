0178b22a97e7fda49c7985244133255465373ae0
lehtite <tero.lehtinen@logisnext.eu>
2023-04-24 18:04:39 +0300

    Fetch electric prices and sort those

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+
+const apiUrl = 'https://api.porssisahko.net/v1/latest-prices.json';
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    data.prices.sort((a, b) => a.price - b.price);
+
+    console.log(data.prices);
+  });
