0c935ebdb6c067104a65b891410672a0e3bdb388
lehtite <tero.lehtinen@logisnext.eu>
2023-04-25 17:38:27 +0300

    added elprices.js

---
+++ elprices.js
@@ -0,0 +1,17 @@
+// get electric prices and sort them
+const apiUrl = 'https://api.porssisahko.net/v1/latest-prices.json';
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    data.prices.sort((a, b) => a.price - b.price);
+
+    console.log(data.prices);
+  });
+
+
+
+  
+// 
+const now = new Date();
+const utcTime = now.toISOString();
+console.log(utcTime);
