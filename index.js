2f4c9c93af08ac0bf99023568208c44222044f5d
lehtite <tero.lehtinen@logisnext.eu>
2023-04-25 17:38:55 +0300

    removed el prices fetch from here

--- index.js
+++ index.js
@@ -2,18 +2,3 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 
-// get electric prices and sort them
-const apiUrl = 'https://api.porssisahko.net/v1/latest-prices.json';
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    data.prices.sort((a, b) => a.price - b.price);
-
-    console.log(data.prices);
-  });
-
-
-// 
-const now = new Date();
-const utcTime = now.toISOString();
-console.log(utcTime);
